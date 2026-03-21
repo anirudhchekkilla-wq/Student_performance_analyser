0fd18d154d2a69fa58730a50e1e852d78b6aa955
Sujith08-dev <23311a1254@it.sreenidhi.edu.in>
2026-03-21 19:44:01 +0530

    fix db path

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from models import db, Student
+import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(BASE_DIR, "students.db")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
